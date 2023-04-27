128f90adc665febeddf2ad2eb6860f84388cdd19
MidnightTale <midnighttale24@gmail.com>
2023-04-27 12:18:10 +0700

    Folia support, Command

--- src/main/java/xyz/hynse/greenchunk/GreenChunk.java
+++ src/main/java/xyz/hynse/greenchunk/GreenChunk.java
@@ -1,17 +1,37 @@
 package xyz.hynse.greenchunk;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import xyz.hynse.greenchunk.util.SlimeChunkUtil;
 
 public final class GreenChunk extends JavaPlugin {
-
+    public static GreenChunk instance;
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
     }
-
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("slime")) {
+            if (!(sender instanceof Player player)) {
+                sender.sendMessage(ChatColor.RED + "You must be a player to use this command!");
+                return true;
+            }
+            if (player.hasPermission("slime.check")) {
+                player.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+                return true;
+            }
+            if (SlimeChunkUtil.isSlimeChunk(player.getLocation().getBlockX() >> 4, player.getLocation().getBlockZ() >> 4, player.getWorld().getSeed())) {
+                player.sendMessage(ChatColor.GREEN + "You are in a slime chunk!");
+            } else {
+                player.sendMessage(ChatColor.RED + "You are not in a slime chunk!");
+            }
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/xyz/hynse/greenchunk/util/SlimeChunkUtil.java
+++ src/main/java/xyz/hynse/greenchunk/util/SlimeChunkUtil.java
@@ -1,4 +1,11 @@
 package xyz.hynse.greenchunk.util;
 
+import java.util.Random;
+
 public class SlimeChunkUtil {
+    public static boolean isSlimeChunk(int chunkX, int chunkZ, long seed) {
+        int x = chunkX * 16 + 8;
+        int z = chunkZ * 16 + 8;
+        return new Random(seed + (long) (x * x * 0x4c1906) + (long) (x * 0x5ac0dbL) + (long) (z * z) * 0x4307a7L + (long) (z * 0x5f24fL)).nextInt(10) == 0;
+    }
 }
